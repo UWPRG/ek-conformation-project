51cf8df7eb427c5ff299158cd9645e095a524d6f
Josh <jsmith07@uw.edu>
2018-12-12 15:39:29 -0800

    calculate full dssp class fractions

--- analysis/secondary_structure/do_dssp.py
+++ analysis/secondary_structure/do_dssp.py
@@ -27,7 +27,7 @@ temps = [
 
 time_range = (0, 300000)  # ps
 
-for seq_idx in range(5):
+for seq_idx in range(5,6):
     for temp_idx, temp in enumerate(temps):
         # topology
         pdb = f'/Users/joshsmith/Git/ek-conformation-project/analysis/clustering/' \
@@ -38,7 +38,7 @@ for seq_idx in range(5):
         # xtc = '/Users/joshsmith/Git/ek-conformation-project/analysis/practice_data/EK_300_skip_100.xtc'
 
         # load low temp traj and restrict frames to time range of interest
-        traj = md.load(xtc, top=pdb, stride=100)
+        traj = md.load(xtc, top=pdb)
         traj = traj[np.where(traj.time >= time_range[0])]
         traj = traj[np.where(traj.time < time_range[1])]
 
@@ -62,88 +62,117 @@ for seq_idx in range(5):
         ######################
         # calculate contacts #
         ######################
-        top = traj.topology
-        # define possible salt bridges
-        pos_charge_center = top.select("resname LYS and name NZ")
-        neg_charge_center = top.select("resname GLU and name CD")
-        bridges = np.array(
-            list(itertools.product(pos_charge_center, neg_charge_center))
-        )
-        # caluclate distances between charge centers in all frames
-        charge_dists = pd.DataFrame(
-            md.compute_distances(traj, atom_pairs=bridges, periodic=False),
-            index=traj.time,
-        )
-        # calculate number of salt bridges in eah frame
-        dist_cutoff = 0.4
-        is_salt_bridge = (charge_dists < dist_cutoff)
-        # # weight by normalized frame weight
-        # avg_num_salt_bridges = sum(
-        #     frame_weights['norm_wt'] * salt_bridge_mask.sum(axis=1)
+        # top = traj.topology
+        # # define possible salt bridges
+        # pos_charge_center = top.select("resname LYS and name NZ")
+        # neg_charge_center = top.select("resname GLU and name CD")
+        # bridges = np.array(
+        #     list(itertools.product(pos_charge_center, neg_charge_center))
         # )
+        # # caluclate distances between charge centers in all frames
+        # charge_dists = pd.DataFrame(
+        #     md.compute_distances(traj, atom_pairs=bridges, periodic=False),
+        #     index=traj.time,
+        # )
+        # # calculate number of salt bridges in eah frame
+        # dist_cutoff = 0.4
+        # is_salt_bridge = (charge_dists < dist_cutoff)
+        # # # weight by normalized frame weight
+        # # avg_num_salt_bridges = sum(
+        # #     frame_weights['norm_wt'] * salt_bridge_mask.sum(axis=1)
+        # # )
 
 
         ###########
         # do dssp #
         ###########
-
-        structure = pd.DataFrame(
-            md.compute_dssp(traj),
-            index=traj.time,
-        )
-
-        helical_mask = (structure == "H")  # for fraction, add: .mean(axis=1)
-        helix_frxn = (helical_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
-        extended_mask = (structure == "E")  # for fraction, add: .mean(axis=1)
-        extended_frxn = (extended_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
-        coil_mask = (structure == "C")  # for fraction, add: .mean(axis=1)
-        coil_frxn = (coil_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
-        # residue-wise salt bridge + structure
-        salt_bridge_mask = pd.DataFrame(
-            np.zeros_like(structure, dtype=bool),
-            index=traj.time,
-        )
-        # for each frame, set True where salt bridge occurs
-        for t in is_salt_bridge.index:
-            # TODO: vectorize once i figure out how to slice mdtraj topology
-            for atom_idx in bridges[np.where(is_salt_bridge.loc[t, :])]:
-                for atom in atom_idx:
-                    res_id = top.atom(atom).residue.index
-                    salt_bridge_mask.loc[t, res_id] = True
-        # find where salt bridge AND given structure is true
-        helix_and_salt_bridge = (helical_mask * salt_bridge_mask).sum(axis=1)
-        extended_and_salt_bridge = (extended_mask * salt_bridge_mask).sum(axis=1)
-        coil_and_salt_bridge = (coil_mask * salt_bridge_mask).sum(axis=1)
-        # filter to only helical frames and reweight fraction
-        nonzero_helix_frames = helical_mask.sum(axis=1).nonzero()
-        helix_salt_bridge_frxn = (
-            (
-                helix_and_salt_bridge.iloc[nonzero_helix_frames]
-                / helical_mask.sum(axis=1).iloc[nonzero_helix_frames]
-            ) * frame_weights['norm_wt']
-        ).sum()
-
-        nonzero_extended_frames = extended_mask.sum(axis=1).nonzero()
-        extended_salt_bridge_frxn = (
-            (
-                extended_and_salt_bridge.iloc[nonzero_extended_frames]
-                / extended_mask.sum(axis=1).iloc[nonzero_extended_frames]
-            ) * frame_weights['norm_wt']
-        ).sum()
-
-        nonzero_coil_frames = coil_mask.sum(axis=1).nonzero()
-        coil_salt_bridge_frxn = (
-            (
-                coil_and_salt_bridge.iloc[nonzero_coil_frames]
-                / coil_mask.sum(axis=1).iloc[nonzero_coil_frames]
-            ) * frame_weights['norm_wt']
-        ).sum()
+        #
+        # structure = pd.DataFrame(
+        #     md.compute_dssp(traj),
+        #     index=traj.time,
+        # )
+        #
+        # helical_mask = (structure == "H")  # for fraction, add: .mean(axis=1)
+        # helix_frxn = (helical_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+        # extended_mask = (structure == "E")  # for fraction, add: .mean(axis=1)
+        # extended_frxn = (extended_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+        # coil_mask = (structure == "C")  # for fraction, add: .mean(axis=1)
+        # coil_frxn = (coil_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+        # # residue-wise salt bridge + structure
+        # salt_bridge_mask = pd.DataFrame(
+        #     np.zeros_like(structure, dtype=bool),
+        #     index=traj.time,
+        # )
+        # # for each frame, set True where salt bridge occurs
+        # for t in is_salt_bridge.index:
+        #     # TODO: vectorize once i figure out how to slice mdtraj topology
+        #     for atom_idx in bridges[np.where(is_salt_bridge.loc[t, :])]:
+        #         for atom in atom_idx:
+        #             res_id = top.atom(atom).residue.index
+        #             salt_bridge_mask.loc[t, res_id] = True
+        # # find where salt bridge AND given structure is true
+        # helix_and_salt_bridge = (helical_mask * salt_bridge_mask).sum(axis=1)
+        # extended_and_salt_bridge = (extended_mask * salt_bridge_mask).sum(axis=1)
+        # coil_and_salt_bridge = (coil_mask * salt_bridge_mask).sum(axis=1)
+        # # filter to only helical frames and reweight fraction
+        # nonzero_helix_frames = helical_mask.sum(axis=1).nonzero()
+        # helix_salt_bridge_frxn = (
+        #     (
+        #         helix_and_salt_bridge.iloc[nonzero_helix_frames]
+        #         / helical_mask.sum(axis=1).iloc[nonzero_helix_frames]
+        #     ) * frame_weights['norm_wt']
+        # ).sum()
+        #
+        # nonzero_extended_frames = extended_mask.sum(axis=1).nonzero()
+        # extended_salt_bridge_frxn = (
+        #     (
+        #         extended_and_salt_bridge.iloc[nonzero_extended_frames]
+        #         / extended_mask.sum(axis=1).iloc[nonzero_extended_frames]
+        #     ) * frame_weights['norm_wt']
+        # ).sum()
+        #
+        # nonzero_coil_frames = coil_mask.sum(axis=1).nonzero()
+        # coil_salt_bridge_frxn = (
+        #     (
+        #         coil_and_salt_bridge.iloc[nonzero_coil_frames]
+        #         / coil_mask.sum(axis=1).iloc[nonzero_coil_frames]
+        #     ) * frame_weights['norm_wt']
+        # ).sum()
 
         # ###########
         # # do dssp #
         # ###########
-        #
-        # structure = md.compute_dssp(traj)
+
+        structure = pd.DataFrame(
+            md.compute_dssp(traj, simplified=False),
+            index=traj.time,
+        )
+
+        alpha_mask = (structure == "H")  # for fraction, add: .mean(axis=1)
+        alpha_frxn = (alpha_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        bridge_mask = (structure == "B")  # for fraction, add: .mean(axis=1)
+        bridge_frxn = (bridge_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        extended_mask = (structure == "E")  # for fraction, add: .mean(axis=1)
+        extended_frxn = (extended_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        three_ten_mask = (structure == "G")  # for fraction, add: .mean(axis=1)
+        three_ten_frxn = (three_ten_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        pi_mask = (structure == "I")  # for fraction, add: .mean(axis=1)
+        pi_frxn = (pi_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        turn_mask = (structure == "T")  # for fraction, add: .mean(axis=1)
+        turn_frxn = (turn_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        bend_mask = (structure == "S")  # for fraction, add: .mean(axis=1)
+        bend_frxn = (bend_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+        coil_mask = (structure == " ")  # for fraction, add: .mean(axis=1)
+        coil_frxn = (coil_mask.mean(axis=1) * frame_weights['norm_wt']).sum()
+
+
         # structure[structure == "H"] = 0.0
         # structure[structure == "E"] = 1.0
         # structure[structure == "C"] = 2.0
@@ -151,11 +180,16 @@ for seq_idx in range(5):
         # plt.imshow(structure.T[:, ::500])
         # plt.show()
 
-        with open(f'structure_{sequences[seq_idx]}.txt', mode='a') as f:
+        with open(f'full_struct_{sequences[seq_idx]}.txt', mode='a') as f:
             print(
                 temp,
+                alpha_frxn,
+                three_ten_frxn,
+                pi_frxn,
+                bridge_frxn,
                 extended_frxn,
-                helix_frxn,
+                turn_frxn,
+                bend_frxn,
                 coil_frxn,
                 file=f
             )
---
+++ analysis/secondary_structure/structure.py
@@ -0,0 +1,78 @@
+import itertools
+import numpy as np
+import pandas as pd
+import mdtraj as md
+
+
+# ####################
+# calculate contacts #
+# ####################
+def calculate_contacts(traj, selection, selection2=None, cutoff=0.4):
+    top = traj.topology
+    # define possible salt bridges
+    sel1_atoms = top.select(selection)
+    sel2_atoms = top.select(selection)
+    if selection2 is not None:
+        sel2_atoms = top.select(selection2)
+    # create list of atom pairs
+    atom_pairs = np.array(
+        list(itertools.product(sel1_atoms, sel2_atoms))
+    )
+    # calculate pairwise distances
+    distances = pd.DataFrame(
+        md.compute_distances(traj, atom_pairs=atom_pairs, periodic=False),
+        index=traj.time,
+    )
+    # return boolean matrix of contacts in each frame
+    contacts = (distances < cutoff)
+    return contacts
+
+
+# ##########
+# do dssp #
+# ##########
+def structure_contact_fraction(traj, contacts, atom_pairs, simplified=True):
+    structure = pd.DataFrame(
+        md.compute_dssp(traj),
+        index=traj.time,
+    )
+
+    helical_mask = (structure == "H")  # for fraction, add: .mean(axis=1)
+    extended_mask = (structure == "E")  # for fraction, add: .mean(axis=1)
+    coil_mask = (structure == "C")  # for fraction, add: .mean(axis=1)
+    # residue-wise salt bridge + structure
+    contacts_mask = pd.DataFrame(
+        np.zeros_like(structure, dtype=bool),
+        index=traj.time,
+    )
+    # for each frame, set True where salt bridge occurs
+    for t in contacts.index:
+        # TODO: vectorize once i figure out how to slice mdtraj topology
+        for atom_idx in atom_pairs[np.where(contacts.loc[t, :])]:
+            for atom in atom_idx:
+                res_id = traj.top.atom(atom).residue.index
+                contacts_mask.loc[t, res_id] = True
+    # find where salt bridge AND given structure is true
+    helix_and_salt_bridge = (helical_mask * contacts_mask).sum(axis=1)
+    extended_and_salt_bridge = (extended_mask * contacts_mask).sum(axis=1)
+    coil_and_salt_bridge = (coil_mask * contacts_mask).sum(axis=1)
+
+    # filter to only helical frames and reweight fraction
+    nonzero_helix_frames = helical_mask.sum(axis=1).nonzero()
+    helix_salt_bridge_frxn = (
+            helix_and_salt_bridge.iloc[nonzero_helix_frames]
+            / helical_mask.sum(axis=1).iloc[nonzero_helix_frames]
+    )
+    nonzero_extended_frames = extended_mask.sum(axis=1).nonzero()
+    extended_salt_bridge_frxn = (
+        extended_and_salt_bridge.iloc[nonzero_extended_frames]
+        / extended_mask.sum(axis=1).iloc[nonzero_extended_frames]
+    )
+    nonzero_coil_frames = coil_mask.sum(axis=1).nonzero()
+    coil_salt_bridge_frxn = (
+        coil_and_salt_bridge.iloc[nonzero_coil_frames]
+        / coil_mask.sum(axis=1).iloc[nonzero_coil_frames]
+    )
+    # TODO: instead, loop through structure types and build df to return
+    return helix_salt_bridge_frxn, extended_salt_bridge_frxn, coil_salt_bridge_frxn
+
